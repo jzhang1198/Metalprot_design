61a592a7385bcbf12ea98a8383bdfd2231e53dac
Jonathan Zhang <jzhang1198@dev1.wynton.ucsf.edu>
2022-06-09 14:18:31 -0700

    Update for wynton

--- job_scripts/activate_env.sh
+++ job_scripts/activate_env.sh
@@ -5,6 +5,6 @@
 
 # The sole purpose of this bash script is to set up the virtual environment to run your jobs in. 
 
-conda activate Metalprot_learning
+conda activate Metalprot_design
 
 $@ ${SGE_TASK_ID}
